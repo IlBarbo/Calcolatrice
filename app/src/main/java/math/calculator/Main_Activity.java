87ab593ad6c84bc16c40810666850a1fcbef28f8
Giuseppe barbolla <barbolla.giuseppe00@gmail.com>
2021-07-19 21:32:03 +0200

    Aggiunta avvio bottoni

--- app/src/main/java/math/calculator/Main_Activity.java
+++ app/src/main/java/math/calculator/Main_Activity.java
@@ -1,5 +1,6 @@
 package math.calculator;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,14 +17,18 @@ public class Main_Activity extends AppCompatActivity {
         final EditText nickname= findViewById(R.id.nickname);
         Button accesbutton= findViewById(R.id.Access);
 
-        if(nickname.getText().toString().length()<5){
 
-        }
 
         accesbutton.setOnClickListener(new View.OnClickListener() { //utilizzo bottone
             @Override
             public void onClick(View v) {
+                if(nickname.getText().toString().length()>5){
+                    Intent apricalc= new Intent(Main_Activity.this,Activity_calc.class);
+                    startActivity(apricalc);
+                }
+                else{ //to do pop up per avvisare lútente che la lunghezza minima e 5
 
+                }
             }
         });
     }
